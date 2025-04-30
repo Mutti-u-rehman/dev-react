9e7dda9dcf26404f8c1c4f874c31f74ef0060c3f
Mutti u rehman <mutti.rehman@adib.com>
2025-04-30 09:11:04 +0400

    card for expension panel

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -22,7 +22,34 @@ const RestaurantMenu = () => {
     <>
       <div className="flex flex-col justify-center">
         <img src={'sdfsd'} alt="Main image of the restaurant" />
-        // <h1>{menuInfo[0]?.card?.card?.text}</h1>
+        <h1>{menuInfo[0]?.card?.card?.text}</h1>
+
+        <div className="flex flex-col m-4 border-2 border-gray-300 rounded-lg p-4">
+
+          {/* header for expansion panel */}
+          <div className="flex flex-row justify-between mb-6">
+            <p className="bold">Heading</p>
+            <div className="">↓</div>
+          </div>
+
+          {/* body of expansion panel */}
+          <div className="flex justify-between">
+
+            {/* panel content */}
+            <div className="flex">
+              <img src="src" alt="Restaurant image" />
+              <div className="flex flex-col">
+                <h2 className="bold">Restaurant Name</h2>
+                <p>Restaurant Address</p>
+                <p>Rating</p>
+              </div>
+            </div>
+            
+            {/* panel action */}
+            <button className="bg-red-500 text-white rounded-lg px-4 py-2">delete</button>
+          </div>
+
+        </div>
 
 
 
